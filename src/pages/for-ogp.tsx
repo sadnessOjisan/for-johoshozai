6b0d9b09e4f8191ba1e595ab481f8f1b17b98bd2
sadnessOjisan <sadness.ojisan@gmail.com>
2022-12-06 19:15:53 +0900

    Fix query

--- src/pages/for-ogp.tsx
+++ src/pages/for-ogp.tsx
@@ -4,21 +4,21 @@ import { useEffect, useState } from "react";
 
 export default () => {
   const router = useRouter();
-  const query = router.query["selected"];
+  const { selected } = router.query;
   const host = "https://4johoshozai.vercel.app";
 
   return (
     <div>
       <Head>
         <meta property="og:title" content="4johoshozai" />
-        <meta property="og:description" content={`${query} | 4johoshozai`} />
+        <meta property="og:description" content={`${selected} | 4johoshozai`} />
         <meta
           property="og:image"
-          content={`${host}/api/image?selected=${query}`}
+          content={`${host}/api/image?selected=${selected}`}
         />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <img src={`${host}/api/image?selected=${query}`} />
+      <img src={`${host}/api/image?selected=${selected}`} />
     </div>
   );
 };
